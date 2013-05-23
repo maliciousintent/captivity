1b9395fc8c2120dad4702fd9dc4eae2ea38d0048
Simone <simone@plasticpanda.com>
2013-05-23 15:07:54 +0200

    Tracking initialize

--- routes/scorm_api.js
+++ routes/scorm_api.js
@@ -1,11 +1,16 @@
-/*jshint node:true, laxcomma:true, indent:2, eqnull:true, es5:true */
+/*jshint node:true, laxcomma:true, indent:2, eqnull:true */
 
 'use strict';
 
 var db = require('nano')(process.env.DATABASE_URL)
   , async = require('async')
   , sugar = require('sugar')
-  , clog = require('clog');
+  , clog = require('clog')
+  , useragent_parser = require('useragent_parser')
+  , Boom = require('boom')
+  , _json = function (res, status, obj) {
+      res.status(status).json(obj);
+    };
 
 /*  
  * SCORM DATA QUICK REFERENCE
@@ -22,13 +27,79 @@ var db = require('nano')(process.env.DATABASE_URL)
  * cmi.total_time          only LMS, r/o
  */
 
-function postSCORM(req, res, next) {
-  res.status(200).json({ error: 'E_NOT_IMPLEMENTED', message: 'LMS does not implement this function.' });
+var _getLastReport = function (user_id, course_id, callback) {
+  db.get('_design/lms/_list/last-report-event/reports', { 
+    startkey: [user_id, course_id, {}], 
+    endkey: [user_id, course_id, null], 
+    include_docs: true,
+    descending: true 
+  }, function (err, body) {
+    if (err) {
+      callback(err);
+    } else {
+      clog.warn('Last report', body[0]);
+      callback(null, body[0]);
+    }
+  });
+};
+
+
+
+function initialize(req, res, next) {
+  var user_id = req.param('user_id')
+    , course_id = req.param('course_id');
+    
+  _getLastReport(user_id, course_id, function (err, report) {
+    if (typeof report.sco_data !== 'object') report.sco_data = {};
+    
+    db.atomic('lms', 'report', undefined, {
+      course_id: report.course_id
+    , user_id: report.user_id
+    , event_type: 'initialize'
+    , event_description: 'Corso caricato'
+    , sco_data: report.sco_data
+    , extra: { 'user-agent': useragent_parser.prettyParse(req.headers['user-agent']) }
+    , created_on: new Date()
+    }, function (err) {
+      if (err) {
+        clog.error('SCORM.Initialize error', err);
+        next(Boom.internal('SCORM.Initialize error'));
+      }
+      
+      _json(res, 200, { ok: true, x_captivity_message: 'Captivity Ready' });
+    });
+    
+  });
 }
- 
- 
+
+
+function terminate(req, res, next) {
+  _json(res, 200, { ok: true, x_captivity_message: 'Bye bye.' });
+}
+
+
+function get(req, res, next) {
+  var user_id = req.param('user_id')
+    , course_id = req.param('course_id')
+    , key = req.param('key')
+    , value = req.param('value');
+    
+  
+}
+
+
+function noop(req, res) {
+  _json(res, 404, { noop: true });
+}
+
+
 module.exports = function (app) {
-  var PREFIX = '/SCORM';
-  app.post(PREFIX + '/*', postSCORM);
+  var PREFIX = '/SCORM/:user_id/:course_id';
+  
+  app.post(PREFIX + '/Initialize', initialize);
+  app.post(PREFIX + '/Terminate', terminate);
+  app.post(PREFIX + '/Set', noop);
+  app.post(PREFIX + '/Get', noop);
+  app.post(PREFIX + '/Commit', noop);
 };
  
